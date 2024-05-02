4e4a0c24147ba93278f36d048b3c2a21386ff8a5
AndriusSurvila <145654904+AndriusSurvila@users.noreply.github.com>
2024-05-02 19:34:46 +0300

    vehicle

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,38 +4,65 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float maxSpeed = 100f;
+    public float acceleration = 1.0f;
+    public float brakeAcceleration = 3f;
     public float rotateSpeed = 1.0f;
-
+    public float reverseAcceleration = 1f;
     Rigidbody rb;
     public AudioSource engineSound;
+    public AnimationCurve pitchCurve;
+    public AnimationCurve rotateSpeedCurve;
+    float speedRatio;
+    public float sightDrag = 1f;
+    public float drag = 5f;
+    public bool isAccelerating;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         engineSound = GetComponent<AudioSource>();
+        rb.maxLinearVelocity = maxSpeed;
     }
 
     void Update()
     {
-        print((int)(rb.velocity.magnitude * 3.6f) + "km/h");
+        print((int)(rb.velocity.magnitude) + "m/s");
 
-        var speedRatio = rb.velocity.magnitude / speed;
-        engineSound.pitch = 1f + 2f * speedRatio;
+        speedRatio = rb.velocity.magnitude / maxSpeed;
+        engineSound.pitch = pitchCurve.Evaluate(speedRatio);
+
+        var localVelocity = transform.InverseTransformVector(rb.velocity);
+
+        rb.velocity += -transform.right * localVelocity.x * sightDrag * Time.deltaTime;
+
+        if (!isAccelerating)
+        {
+            rb.velocity += -transform.forward * localVelocity.z * drag * Time.deltaTime;
+        }
+        
+        if (Mathf.Abs(localVelocity.normalized.x) > 0.5f)
+        {
+            print("drift");
+        }
+
+        isAccelerating = false;
     }
 
     public void Steer(float value)
     {
-        transform.Rotate(0, value * rotateSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, value * rotateSpeed * rotateSpeedCurve.Evaluate(speedRatio) * speedRatio * Time.deltaTime, 0);
     }
 
     public void Accelerate()
     {
-        rb.velocity += transform.forward * speed * Time.deltaTime;
+        rb.velocity += transform.forward * acceleration * Time.deltaTime;
+        isAccelerating = true;
     }
 
     public void Brake()
     {
-
+        var acc = speedRatio > 0 ? brakeAcceleration : reverseAcceleration;
+        rb.velocity += -transform.forward * acc * Time.deltaTime;
     }
 }
